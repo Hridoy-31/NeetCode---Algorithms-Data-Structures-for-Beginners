4b6ac6b75070f8bb40240e5e7f5e65c83c32a06e
S.M. Raihanul Bashir Hridoy <raihanulbashirhridoy@gmail.com>
2024-08-13 20:36:48 +0600

    Brute + Optimal

--- 02. Arrays/03. Static Arrays/LC26.cpp
+++ 02. Arrays/03. Static Arrays/LC26.cpp
@@ -1,8 +1,48 @@
 /*
 LeetCode 26: Remove Duplicates from Sorted Array
+*/
+
+
+/*
+Bruteforce approach:
+
+Time complexity: O(n log n) + O(n) = O(n log n)
+    O(n log n): O(n) comes from iterating over all the elements of nums vector. 
+                O(log n) comes from inserting the elements to the set. 
+                    The set is implemented by balanced binary search tree.
+                    Set stores only unique elements in ascending order.
+    O(n): This O(n) comes from iterating over all the elements in the set to
+	      put the elements back in the nums vector at right positions.
+
+    The dominant factor between these two is O(n log n). So, the total time complexity
+    of this function is O(n log n). 
+
+Space complexity: O(n)
+*/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set <int> result;
+        for (auto element : nums) {
+            result.insert(element);
+        }
+        int i = 0;
+        for (auto& element : result) {
+            nums[i] = element;
+            i++;  
+        }
+        return result.size();
+    }
+};
+
+
+/*
+Optimal approach:
 
 Time complexity: O(n)
 Space complexity: O(1)
+    O(1): Because we are modifying the nums vector in-place. So, no extra space needed.	
 */
 
 class Solution {
